eb304d047f28130fb43b94b122fa1a16d6c2ae68
Brian Park <brian@xparks.net>
2018-07-19 17:57:48 -0700

    Add copyright verbiage to cli/* files

--- src/ace_routine/cli/CommandDispatcher.h
+++ src/ace_routine/cli/CommandDispatcher.h
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2018 Brian T. Park
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef ACE_ROUTINE_COMMAND_DISPATCHER_H
 #define ACE_ROUTINE_COMMAND_DISPATCHER_H
 
@@ -64,10 +88,9 @@ class CommandDispatcher: public Coroutine {
             const CommandHandler* const* handlers):
         mSerialReader(serialReader),
         mNumCommands(numCommands),
-        mComandHandlers(handlers) {
-    }
+        mComandHandlers(handlers) {}
 
-  protected:
+  private:
     static const uint8_t STATUS_SUCCESS = 0;
     static const uint8_t STATUS_BUFFER_OVERFLOW = 1;
     static const uint8_t STATUS_FLUSH_TO_EOL = 2;

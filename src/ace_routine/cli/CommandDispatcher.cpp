912d6993a942ceb76c215a54610244a295e3a68f
Brian Park <brian@xparks.net>
2018-07-19 17:44:00 -0700

    Move implementation from CommandDispatcher.h to CommandDispatcher.cpp

--- src/ace_routine/cli/CommandDispatcher.cpp
+++ src/ace_routine/cli/CommandDispatcher.cpp
@@ -5,5 +5,114 @@ namespace cli {
 
 const char CommandDispatcher::DELIMS[] = " \t\n";
 
+void CommandDispatcher::printLineError(const char* line, uint8_t statusCode) {
+  if (statusCode == STATUS_BUFFER_OVERFLOW) {
+    Serial.print(F("BufferOverflow: "));
+    Serial.println(line);
+  } else if (statusCode == STATUS_FLUSH_TO_EOL) {
+    Serial.print(F("FlushToEOL: "));
+    Serial.println(line);
+  } else {
+    Serial.print(F("UnknownError: "));
+    Serial.print(statusCode);
+    Serial.print(": ");
+    Serial.println(line);
+  }
+}
+
+/** Handle the 'help' command. */
+void CommandDispatcher::helpCommandHandler(int argc, const char** argv) {
+  if (argc == 2) {
+    const char* cmd = argv[1];
+    if (strcmp(cmd, "help") == 0) {
+      Serial.println(F("Usage: help [command]"));
+      return;
+    }
+
+    for (uint8_t i = 0; i < mNumCommands; i++) {
+      const CommandHandler* handler = mComandHandlers[i];
+      if (strcmp(handler->getName(), cmd) == 0) {
+        Serial.print("Usage: ");
+        Serial.print(cmd);
+        Serial.print(' ');
+        Serial.println(handler->getHelpString());
+        return;
+      }
+    }
+    Serial.print(F("Unknown command: "));
+    Serial.println(cmd);
+  } else {
+    Serial.println(F("Usage: help [command]"));
+    Serial.print(F("Commands: help "));
+    for (uint8_t i = 0; i < mNumCommands; i++) {
+      const CommandHandler* handler = mComandHandlers[i];
+      Serial.print(handler->getName());
+      Serial.print(' ');
+    }
+    Serial.println();
+  }
+}
+
+/** Tokenize the given line and run the command handler. */
+void CommandDispatcher::runCommand(char* line) {
+  // Tokenize the line.
+  const char* argv[ARGV_SIZE];
+  int argc = tokenize(line, ARGV_SIZE, argv);
+  if (argc == 0) return;
+  const char* cmd = argv[0];
+
+  // The 'help' command is built-in.
+  if (strcmp(cmd, "help") == 0) {
+    helpCommandHandler(argc, argv);
+    return;
+  }
+
+  // Look for an entry in the handlers.
+  // NOTE: this is currently a linear scan O(N) which is good enough for
+  // small number of commands. If we sorted the handlers, we could do
+  // a binary search for O(log(N)) and handle large number of commands.
+  for (uint8_t i = 0; i < mNumCommands; i++) {
+    const CommandHandler* handler = mComandHandlers[i];
+    if (strcmp(handler->getName(), cmd) == 0) {
+      handler->run(argc, argv);
+      return;
+    }
+  }
+
+  Serial.print(F("Unknown command: "));
+  Serial.println(cmd);
+}
+
+/** Tokenize the line, returning the number of tokens. */
+int CommandDispatcher::tokenize(char* line, int argvSize, const char** argv) {
+  char* token = strtok(line, DELIMS);
+  int argc = 0;
+  while (token != nullptr && argc < argvSize) {
+    argv[argc] = token;
+    argc++;
+    token = strtok(nullptr, DELIMS);
+  }
+  return argc;
+}
+
+int CommandDispatcher::runRoutine() {
+  bool isError;
+  char* line;
+  COROUTINE_LOOP() {
+    COROUTINE_AWAIT(mSerialReader.getLine(&isError, &line));
+
+    if (isError) {
+      printLineError(line, STATUS_BUFFER_OVERFLOW);
+      while (isError) {
+        COROUTINE_AWAIT(mSerialReader.getLine(&isError, &line));
+        printLineError(line, STATUS_FLUSH_TO_EOL);
+      }
+      continue;
+    }
+
+    runCommand(line);
+  }
+}
+
 }
 }
--- src/ace_routine/cli/CommandDispatcher.h
+++ src/ace_routine/cli/CommandDispatcher.h
@@ -9,21 +9,20 @@ namespace ace_routine {
 namespace cli {
 
 /**
- * Abstract class of command handlers.
+ * Abstract class for command handlers.
  */
 class CommandHandler {
   public:
     /**
      * Constructor.
      *
-     * @param helpString the "usage" string, excluding the name of the command
-     * itself to save space.
-     * @param name Name of the command will be automatically added by the 'help
-     * command' handler.
+     * @param helpString The "usage" string, excluding the name of the command
+     *        itself to save space.
+     * @param name Name of the command will be automatically added by the
+     *        'help' handler.
      */
     CommandHandler(const char* name, const char* helpString):
-        mName(name),
-        mHelpString(helpString) {}
+        mName(name), mHelpString(helpString) {}
 
     virtual void run(int argc, const char** argv) const = 0;
 
@@ -56,8 +55,8 @@ class CommandDispatcher: public Coroutine {
      * Constructor.
      *
      * @param serialReader Instance of SerialReader.
-     * @param numCommands Number of entries in handlers
-     * @param handlers An array of CommandHandler
+     * @param numCommands Number of entries in handlers.
+     * @param handlers An array of CommandHandler pointers.
      */
     CommandDispatcher(
             SerialReader& serialReader,
@@ -74,111 +73,19 @@ class CommandDispatcher: public Coroutine {
     static const uint8_t STATUS_FLUSH_TO_EOL = 2;
     static const char DELIMS[];
 
-    void printLineError(const char* line, uint8_t statusCode) {
-      if (statusCode == STATUS_BUFFER_OVERFLOW) {
-        Serial.print(F("BufferOverflow: "));
-        Serial.println(line);
-      } else if (statusCode == STATUS_FLUSH_TO_EOL) {
-        Serial.print(F("FlushToEOL: "));
-        Serial.println(line);
-      } else {
-        Serial.println(F("UnknownError"));
-      }
-    }
+    /** Print the error caused by the given line. */
+    void printLineError(const char* line, uint8_t statusCode);
 
     /** Handle the 'help' command. */
-    void helpCommandHandler(int argc, const char** argv) {
-      if (argc == 2) {
-        const char* cmd = argv[1];
-        if (strcmp(cmd, "help") == 0) {
-          Serial.println(F("Usage: help [command]"));
-          return;
-        }
-
-        for (uint8_t i = 0; i < mNumCommands; i++) {
-          const CommandHandler* handler = mComandHandlers[i];
-          if (strcmp(handler->getName(), cmd) == 0) {
-            Serial.print("Usage: ");
-            Serial.print(cmd);
-            Serial.print(' ');
-            Serial.println(handler->getHelpString());
-            return;
-          }
-        }
-        Serial.print(F("Unknown command: "));
-        Serial.println(cmd);
-      } else {
-        Serial.println(F("Usage: help [command]"));
-        Serial.print(F("Commands: help "));
-        for (uint8_t i = 0; i < mNumCommands; i++) {
-          const CommandHandler* handler = mComandHandlers[i];
-          Serial.print(handler->getName());
-          Serial.print(' ');
-        }
-        Serial.println();
-      }
-    }
+    void helpCommandHandler(int argc, const char** argv);
 
     /** Tokenize the given line and run the command handler. */
-    void runCommand(char* line) {
-      // Tokenize the line.
-      const char* argv[ARGV_SIZE];
-      int argc = tokenize(line, ARGV_SIZE, argv);
-      if (argc == 0) return;
-      const char* cmd = argv[0];
-
-      // The 'help' command is built-in.
-      if (strcmp(cmd, "help") == 0) {
-        helpCommandHandler(argc, argv);
-        return;
-      }
-
-      // Look for an entry in the handlers.
-      // NOTE: this is currently a linear scan O(N) which is good enough for
-      // small number of commands. If we sorted the handlers, we could do
-      // a binary search for O(log(N)) and handle large number of commands.
-      for (uint8_t i = 0; i < mNumCommands; i++) {
-        const CommandHandler* handler = mComandHandlers[i];
-        if (strcmp(handler->getName(), cmd) == 0) {
-          handler->run(argc, argv);
-          return;
-        }
-      }
-
-      Serial.print(F("Unknown command: "));
-      Serial.println(cmd);
-    }
+    void runCommand(char* line);
 
     /** Tokenize the line, returning the number of tokens. */
-    int tokenize(char* line, int argvSize, const char** argv) {
-      char* token = strtok(line, DELIMS);
-      int argc = 0;
-      while (token != nullptr && argc < argvSize) {
-        argv[argc] = token;
-        argc++;
-        token = strtok(nullptr, DELIMS);
-      }
-      return argc;
-    }
+    int tokenize(char* line, int argvSize, const char** argv);
 
-    virtual int runRoutine() override {
-      bool isError;
-      char* line;
-      COROUTINE_LOOP() {
-        COROUTINE_AWAIT(mSerialReader.getLine(&isError, &line));
-
-        if (isError) {
-          printLineError(line, STATUS_BUFFER_OVERFLOW);
-          while (isError) {
-            COROUTINE_AWAIT(mSerialReader.getLine(&isError, &line));
-            printLineError(line, STATUS_FLUSH_TO_EOL);
-          }
-          continue;
-        }
-
-        runCommand(line);
-      }
-    }
+    virtual int runRoutine() override;
 
     SerialReader& mSerialReader;
     const uint8_t mNumCommands;

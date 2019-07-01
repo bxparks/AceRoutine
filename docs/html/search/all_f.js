66f89e8e9ea3fc3abb3c626f3d4c1d6eed3349a1
Brian Park <brian@xparks.net>
2019-07-01 16:26:20 -0700

    docs: Update doxygen docs for 0.2.1

--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_7ecommanddispatcher',['~CommandDispatcher',['../classace__routine_1_1cli_1_1CommandDispatcher.html#aca8a594379610bac6e33f7b0791f124c',1,'ace_routine::cli::CommandDispatcher']]]
+  ['_7ecommanddispatcher',['~CommandDispatcher',['../classace__routine_1_1cli_1_1CommandDispatcher.html#aca8a594379610bac6e33f7b0791f124c',1,'ace_routine::cli::CommandDispatcher']]],
+  ['_7ecoroutine',['~Coroutine',['../classace__routine_1_1Coroutine.html#af30b8f647b2dda74b65261aa0c86d6e7',1,'ace_routine::Coroutine']]]
 ];

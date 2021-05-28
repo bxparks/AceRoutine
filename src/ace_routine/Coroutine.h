bxparks/AceRoutine

commit 50b2c70dc49e3c75ded62d0831fc916fbb2dc533

src/Coroutine.h: Simplify switch statement in isDelayExpired(), saving about 50 bytes of flash on AVR

src/ace_routine/Coroutine.h
@@ -343,23 +343,22 @@ class CoroutineTemplate {
 
     /** Check if delay time is over. */
     bool isDelayExpired() const {
+      uint16_t elapsed;
       switch (mDelayType) {
-        case kDelayTypeMillis: {
-          uint16_t elapsedMillis = coroutineMillis() - mDelayStart;
-          return elapsedMillis >= mDelayDuration;
-        }
-        case kDelayTypeMicros: {
-          uint16_t elapsedMicros = coroutineMicros() -  mDelayStart;
-          return elapsedMicros >= mDelayDuration;
-        }
-        case kDelayTypeSeconds: {
-          uint16_t elapsedSeconds = coroutineSeconds() -  mDelayStart;
-          return elapsedSeconds >= mDelayDuration;
-        }
-        default:
-          // This should never happen.
+        case kDelayTypeMillis:
+          elapsed = coroutineMillis();
+          break;
+        case kDelayTypeMicros:
+          elapsed = coroutineMicros();
+          break;
+        case kDelayTypeSeconds:
+          elapsed = coroutineSeconds();
+          break;
+        default: // This should never happen.
           return true;
       }
+
+      return elapsed >= mDelayDuration;
     }
 
     /** The coroutine was suspended with a call to suspend(). */

examples/MemoryBenchmark/MemoryBenchmark.ino
@@ -18,9 +18,11 @@
 #define FEATURE_BASELINE 0
 #define FEATURE_MANUAL_DELAY_LOOP 1
 #define FEATURE_ONE_COROUTINE 2
-#define FEATURE_TWO_COROUTINES 3
-#define FEATURE_SCHEDULER_ONE_COROUTINE 4
-#define FEATURE_SCHEDULER_TWO_COROUTINES 5
+#define FEATURE_ONE_COROUTINE_DELAY_SECONDS 3
+#define FEATURE_TWO_COROUTINES 4
+#define FEATURE_TWO_COROUTINES_DELAY_SECONDS 5
+#define FEATURE_SCHEDULER_ONE_COROUTINE 6
+#define FEATURE_SCHEDULER_TWO_COROUTINES 7
 
 #if FEATURE != FEATURE_BASELINE
   #include <AceRoutine.h>
@@ -55,6 +57,15 @@ volatile int disableCompilerOptimization = 0;
     }
   }
 
+#elif FEATURE == FEATURE_ONE_COROUTINE_DELAY_SECONDS
+
+  COROUTINE(a) {
+    COROUTINE_LOOP() {
+      disableCompilerOptimization = 1;
+      COROUTINE_DELAY_SECONDS(10);
+    }
+  }
+
 #elif FEATURE == FEATURE_TWO_COROUTINES
 
   COROUTINE(a) {
@@ -68,6 +79,22 @@ volatile int disableCompilerOptimization = 0;
     COROUTINE_LOOP() {
       disableCompilerOptimization = 1;
       COROUTINE_DELAY(10);
+    }
+  }
+
+#elif FEATURE == FEATURE_TWO_COROUTINES_DELAY_SECONDS
+
+  COROUTINE(a) {
+    COROUTINE_LOOP() {
+      disableCompilerOptimization = 1;
+      COROUTINE_DELAY_SECONDS(10);
+    }
+  }
+
+  COROUTINE(b) {
+    COROUTINE_LOOP() {
+      disableCompilerOptimization = 1;
+      COROUTINE_DELAY_SECONDS(10);
     }
   }
 
@@ -119,7 +146,12 @@ void loop() {
   manualDelayLoop();
 #elif FEATURE == FEATURE_ONE_COROUTINE
   a.runCoroutine();
+#elif FEATURE == FEATURE_ONE_COROUTINE_DELAY_SECONDS
+  a.runCoroutine();
 #elif FEATURE == FEATURE_TWO_COROUTINES
+  a.runCoroutine();
+  b.runCoroutine();
+#elif FEATURE == FEATURE_TWO_COROUTINES_DELAY_SECONDS
   a.runCoroutine();
   b.runCoroutine();
 #elif FEATURE == FEATURE_SCHEDULER_ONE_COROUTINE

examples/MemoryBenchmark/collect.sh
@@ -16,7 +16,7 @@
 set -eu
 
 PROGRAM_NAME='MemoryBenchmark.ino'
-NUM_FEATURES=5 # excluding FEATURE_BASELINE
+NUM_FEATURES=7 # excluding FEATURE_BASELINE
 
 # Assume that https://github.com/bxparks/AUniter is installed as a
 # sibling project to AceRoutine.

examples/MemoryBenchmark/generate_table.awk
@@ -9,9 +9,11 @@ BEGIN {
   labels[0] = "Baseline"
   labels[1] = "Manual Delay Loop"
   labels[2] = "One Coroutine"
-  labels[3] = "Two Coroutines"
-  labels[4] = "Scheduler, One Coroutine"
-  labels[5] = "Scheduler, Two Coroutines"
+  labels[3] = "One Coroutine, Delay Seconds"
+  labels[4] = "Two Coroutines"
+  labels[5] = "Two Coroutine, Delay Seconds"
+  labels[6] = "Scheduler, One Coroutine"
+  labels[7] = "Scheduler, Two Coroutines"
   record_index = 0
 }
 {

commit 891de242e3c538aaadd28cdb0f85f214cb9dc8a2

src/Coroutine.h: Fix bug caused by incorrect refactoring in 50b2c70

src/ace_routine/Coroutine.h
@@ -314,18 +314,18 @@ class CoroutineTemplate {
 
     /** Check if delay time is over. */
     bool isDelayExpired() const {
-      uint16_t elapsed;
+      uint16_t now;
       switch (mDelayType) {
         case kDelayTypeMillis:
-          elapsed = coroutineMillis();
+          now = coroutineMillis();
           break;
         case kDelayTypeMicros:
-          elapsed = coroutineMicros();
+          now = coroutineMicros();
           break;
         default: // This should never happen.
           return true;
       }
-
+      uint16_t elapsed = now - mDelayStart;
       return elapsed >= mDelayDuration;
     }
 
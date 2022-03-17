6064073c1dac35ad2cb62e541b3fa28987baf46f
Brian Park <brian@xparks.net>
2022-03-17 10:39:23 -0700

    examples/SoundManager: Remove profiling code; demo code moved to Hello{Coroutine,Scheduler}WithProfiler instead

--- examples/SoundManager/SoundManager.ino
+++ examples/SoundManager/SoundManager.ino
@@ -21,30 +21,12 @@
  * Coroutine soundManager; status: Running
  * First BEEP
  * Second BEEP
- * name          <8us <16us <32us <64us<128us<256us<512us  <1ms  <2ms    >>
- * soundManager  1440     0     1     0     0     0     0     0     1     0
- * 0x55C5AB3CE2  1438     1     2     0     0     0     0     0     0     1
- * soundRoutine  1440     0     0     0     2     0     0     0     0     0
- * {
- * "soundManager":[1440,0,1,0,0,0,0,0,1,0],
- * "0x55C5AB3CE240":[1438,1,2,0,0,0,0,0,0,1],
- * "soundRoutine":[1440,0,0,0,2,0,0,0,0,0]
- * }
  * 
  * Request Boop and wait 5 seconds...
  * Coroutine soundRoutine; status: Yielding
  * Coroutine soundManager; status: Running
  * First BOOP
  * Second BOOP
- * name          <8us <16us <32us <64us<128us<256us<512us  <1ms  <2ms    >>
- * soundManager  1440     0     1     0     0     0     0     0     1     0
- * 0x55C5AB3CE2  1438     1     2     0     0     0     0     0     0     1
- * soundRoutine  1440     0     0     0     2     0     0     0     0     0
- * {
- * "soundManager":[1440,0,1,0,0,0,0,0,1,0],
- * "0x55C5AB3CE240":[1438,1,2,0,0,0,0,0,0,1],
- * "soundRoutine":[1440,0,0,0,2,0,0,0,0,0]
- * }
  * 
  * Request Silence and wait 5 seconds...
  * Coroutine soundRoutine; status: Yielding
@@ -60,44 +42,19 @@
 #include <AceRoutine.h>
 #include "SoundRoutine.h"
 
-using ace_routine::Coroutine;;
 using ace_routine::CoroutineScheduler;
-using ace_routine::LogBinProfiler;
-using ace_routine::LogBinTableRenderer;
-using ace_routine::LogBinJsonRenderer;
 
-SoundRoutine soundRoutine;
 EXTERN_COROUTINE(soundManager);
-
-// Every 5 seconds, print out the elapsed time frequency distribution from the
-// LogBinProfiler.
-COROUTINE(printProfiler) {
-  COROUTINE_LOOP() {
-    LogBinTableRenderer tableRenderer(getRoot());
-    tableRenderer.printTo(Serial, 2, 12, false /*clear*/);
-
-    LogBinJsonRenderer jsonRenderer(getRoot());
-    jsonRenderer.printTo(Serial, 2, 12);
-
-    COROUTINE_DELAY_SECONDS(5);
-  }
-}
+SoundRoutine soundRoutine;
 
 void setup() {
   delay(1000);
   Serial.begin(115200);
   while (!Serial); // needed for Leonardo/Micro
 
-  // Set names using both c-string and f-string for testing purposes.
-  soundRoutine.setName("soundRoutine");
+  // Coroutine name can be ether C-string or F-string.
+  soundRoutine.setName("soundRoutine"); 
   soundManager.setName(F("soundManager"));
-
-  // Don't set the name of 'printProfiler' to verify that the name of the
-  // coroutine becomes the pointer address in hexadecimal.
-  // printProfiler.setName(F("printProfiler"));
-
-  // Attach profilers to all coroutines.
-  LogBinProfiler::createProfilers(Coroutine::getRoot());
 
   CoroutineScheduler::setup();
 }
